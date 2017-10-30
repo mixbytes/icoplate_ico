d893969f131193f8454c1353ea08a52388c4f9ad
Andrey Yakovlev <679008@gmail.com>
2017-10-31 02:56:03 +0300

    Add comments to MultiControlledMixin Fix tests

--- contracts/mixins/MultiControlledMixin.sol
+++ contracts/mixins/MultiControlledMixin.sol
@@ -11,19 +11,21 @@ contract MultiControlledMixin is Ownable {
     event ControllerDisabledForever();
 
     modifier onlyControllers() {
-        require(checkController(msg.sender));
+        require(isController(msg.sender));
         _;
     }
 
+    /// @dev Add address to controllers
     function addController(address _controller) external onlyOwner {
         require(!m_isSetControllerDisabled);
-        require(m_controllers.length < c_MaxControllers + 1);
-        if (!checkController(_controller)){
+        require(m_controllers.length < c_MaxControllers);
+        if (!isController(_controller)){
             m_controllers.push(_controller);
             ControllerAdd(_controller);
         }
     }
 
+    /// @dev Detach address from controllers
     function detachController(address _controller) external onlyOwner {
         require(!m_isSetControllerDisabled);
         for (uint i = 0; i < m_controllers.length; ++i) {
@@ -35,6 +37,7 @@ contract MultiControlledMixin is Ownable {
         }
     }
 
+    /// @dev Detach all controllers and disable adding
     function detachControllersForever() external onlyOwner {
         require(!m_isSetControllerDisabled);
         m_controllers = new address[](0);
@@ -42,13 +45,24 @@ contract MultiControlledMixin is Ownable {
         ControllerDisabledForever();
     }
 
-    function checkController(address _controller) internal returns (bool) {
+    /// @dev Check if address in controllers
+    function isController(address _controller) internal returns (bool) {
         for (uint i = 0; i < m_controllers.length; ++i) {
             if (m_controllers[i] == _controller) {
                 return true;
             }
         }
         return false;
+    }
+
+    /// @notice Gets controllers
+    /// @return memory array of controllers
+    function getControllers() public constant returns (address[]) {
+        address[] memory result = new address[](m_controllers.length);
+        for (uint i = 0; i < m_controllers.length; ++i)
+        result[i] = m_controllers[i];
+
+        return result;
     }
 
     // FIELDS
